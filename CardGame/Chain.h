37c4394e174a4650c727712578fde8fac29d48ab
coollx <45716379+coollx@users.noreply.github.com>
2021-11-23 00:06:39 +0800

    created Chain.h

---
+++ CardGame/Chain.h
@@ -0,0 +1,115 @@
+//
+// Created by Xiang Li on 2021-11-21.
+//
+#include "Card.h"
+#ifndef CARDGAME_CHAIN_H
+#define CARDGAME_CHAIN_H
+
+
+
+namespace cardgame {
+    class IllegalType : public std::exception {
+        const char * what() const throw () {
+            return "Try to put different cards in the same chain";
+        }
+    };
+
+
+    class Chain_Base {
+        protected:
+            //virtual void insertCard(Card*) = 0;
+            std::vector<Card*> cardChain;
+            bool addCard(Card* card) {
+                if (cardChain.size() == 0 || typeid(*card) == typeid(*cardChain.front())) {
+                    cardChain.push_back(card);
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
+
+            virtual void printChain(std::ostream&) const  = 0;
+
+        public:
+            //virtual int sell() = 0;
+            friend std::ostream& operator << (std::ostream& out, const Chain_Base& chain) {
+                chain.printChain(out);
+                return out;
+            };
+
+            virtual int sell() = 0;
+
+            virtual Chain_Base& operator += (Card*) = 0;
+
+
+    };
+
+
+    template<class T>
+    class Chain : public Chain_Base {
+        protected:
+            void printChain(std::ostream& out) const override {
+                //std::cout << "hello" << std::endl;
+                if (cardChain.size() > 0) {
+                    //out << cardChain[0]->getName() << std::endl;
+                    for (auto& card : this->cardChain) {
+                        out << card->getName() << " ";
+                    }
+                }
+                out << std::endl;
+            }
+
+
+        public:
+            //Default constructor
+            Chain() = default;
+
+            /*Constructor*/
+            Chain(std::istream &in, CardFactory *factory) {
+                while (!in.eof()) {
+                    char c = (char) in.get();
+                    if (std::isalpha(c)) {
+                        if(!this->addCard(factory->CreateCard(c))) {
+                            throw IllegalType();
+                        }
+                        //cardChain.push_back();
+                    }
+                }
+            }
+
+            Chain<T>& operator += (Card* card) {
+                if (this->addCard(card)) {
+                    return *this;
+                } else {
+                    throw IllegalType();
+                }
+            }
+
+            int sell() {
+                T card;
+                int ret = 0;
+                int length = cardChain.size();
+                while (length >= card.getCardsPerCoin(1)) {
+                    for (int coin = 4; coin > 0; --coin) {
+                        if (length >= card.getCardsPerCoin(coin)) {
+                            length -= card.getCardsPerCoin(coin);
+                            ret += coin;
+                            break;
+                        }
+                    }
+                }
+                return ret;
+            }
+
+
+
+//            void printChain(std::ostream& out) const {
+//                out << (*this);
+//            }
+
+
+    };
+}
+
+#endif //CARDGAME_CHAIN_H
--- CardGame/main.cpp
+++ CardGame/main.cpp
@@ -4,6 +4,7 @@
 #include "CardFactory.h"
 #include "TradeArea.h"
 #include "Hand.h"
+#include "Chain.h"
 
 using namespace cardgame;
 using namespace std;
@@ -12,11 +13,21 @@ int main() {
     CardFactory *pCardFactory = CardFactory::getFactory();
     CardFactory::Deck deck{};
 
+//    deck.push_back(pCardFactory->CreateCard('b'));
+//    deck.push_back(pCardFactory->CreateCard('B'));
+//    deck.push_back(pCardFactory->CreateCard('C'));
+//    deck.push_back(pCardFactory->CreateCard('R'));
+//    deck.push_back(pCardFactory->CreateCard('g'));
+
     deck.push_back(pCardFactory->CreateCard('b'));
-    deck.push_back(pCardFactory->CreateCard('B'));
-    deck.push_back(pCardFactory->CreateCard('C'));
-    deck.push_back(pCardFactory->CreateCard('R'));
-    deck.push_back(pCardFactory->CreateCard('g'));
+    deck.push_back(pCardFactory->CreateCard('b'));
+    deck.push_back(pCardFactory->CreateCard('b'));
+    deck.push_back(pCardFactory->CreateCard('b'));
+    deck.push_back(pCardFactory->CreateCard('b'));
+
+    for (int i = 0; i < 20; ++i) {
+        deck.push_back(pCardFactory->CreateCard('b'));
+    }
 
     ofstream outfile;
     outfile.open("save.txt");
@@ -26,15 +37,18 @@ int main() {
     ifstream infile;
     infile.open("save.txt");
 
-    Hand *hand = new Hand(infile, pCardFactory);
+    Chain_Base* chain = new Chain<Black>(infile, pCardFactory);
+    *chain += pCardFactory->CreateCard('b');
+    cout << *chain;
+    cout << chain->sell() << endl; //should sell 17 coins
 
-    Card *c = hand->play();
 
-    cout << (*hand)[2]->getName() << endl;
-
-    cout << *hand << endl;
+//    Card *c = hand->play();
+//
+//    cout << (*hand)[2]->getName() << endl;
+//
+//    cout << *hand << endl;
 
     infile.close();
-
     return 0;
 }
